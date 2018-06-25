6fd79d4fcd65afe39a20ce4b58dae4c034473e0c
Jordan Garcia <40546419+Dassjon@users.noreply.github.com>
2018-06-24 19:56:19 -0500

    Add files via upload
    Empezando la practica n.n

---
+++ adivina_en_numero.py
@@ -0,0 +1,31 @@
+number_to_guess = 69
+
+user_number = int(input("Adivina el numero del 1 al 100 kvron: "))
+
+if number_to_guess == user_number:
+    print("has acertado zukulentamente 7u7")
+else:
+    print("que pendejo, no era xDXDXdxDXD")
+    user_number = int(input("intentalo de nuevo: "))
+    if number_to_guess == user_number:
+        print("has acertado zukulentamente 7u7")
+    else:
+        print("tampoco es ese mongolo 7n7")
+        user_number == int(input("intentalo otra vez menso: "))
+        if number_to_guess == user_number:
+            print("has acertado zukulentamente 7u7")
+        else:
+            print("a ver cuando le atinas estupido")
+            user_number == int(input("intentalo otra vez de nuevo una vez mas!: "))
+            if number_to_guess == user_number:
+                print("has acertado zukulentamente 7u7")
+            else:
+                print("y sigues equivocandote melolengo jajaja")
+                user_number == int(input("intentalo otra vez pesao: "))
+                if number_to_guess == user_number:
+                    print("porfin acertaste chingadamadre")
+                else:
+                    print("no we ya te chingaste")
+
+
+
---
+++ test_input.py
@@ -0,0 +1,7 @@
+numero_del_usuario = input ("Dime un numero: ")
+print(numero_del_usuario)
+print ("que puto eres kvron")
+numero_del_usuario2 = input ("bien, ahora dame otro numero cara anchoa: ")
+print(numero_del_usuario2)
+print("neta eso es lo mejor que tienes? 7n7")
+
